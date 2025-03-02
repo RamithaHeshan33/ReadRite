614654c054abfa6822804a1ff3d5c34e9529a149
Ramitha Heshan <st20286184@outlook.cardiffmet.ac.uk>
2025-03-02 19:56:26 +0530

    frontend initialization

---
+++ frontend/src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Navbar() {
+  return (
+    <div>
+      <button class="btn btn-primary" type="submit">Button</button>
+    </div>
+  )
+}
+
+export default Navbar
